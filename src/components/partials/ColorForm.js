8c183db543763af30d6e875f59aaef7e9d810d8d
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-10-25 21:03:00 +0100

    basic color picker added

--- src/components/partials/ColorForm.js
+++ src/components/partials/ColorForm.js
@@ -22,24 +22,36 @@ const ColorForm = () => {
     const onSubmit = (e) => {
         e.preventDefault();
         console.log({primaryColor, secondaryColor})
+        setColor({
+            primaryColor: '',
+            secondaryColor: ''
+        })
     }
     return (
         <div>
             <form className='form' onSubmit={e => onSubmit(e)}>
                 <div className='form-group'>
-                    <input type="text" 
+                    <label htmlFor="primaryColor">Primary Color</label>
+                    <input type="color" 
                     placeholder='Primary Color'
                     name='primaryColor'
                     value={primaryColor}
                     onChange={e => onChange(e)}/>
                 </div>
+               
+                {primaryColor}
+            
                 <div className='form-group'>
-                    <input type="text" 
+                <label htmlFor="secondaryColor">Secondary Color</label>
+                    <input type="color" 
                     placeholder='Secondary Color'
                     name='secondaryColor'
                     value={secondaryColor}
                     onChange={e => onChange(e)}/>
-                </div>
+                </div> 
+                <div>
+                {secondaryColor}
+            </div>
                 <button>Submit</button>
             </form>
         </div>
